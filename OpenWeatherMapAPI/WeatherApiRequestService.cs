99016c337b2743750dbce82b834555e6ef78e2fb
RichardRodi <RichardmRodi@gmail.com>
2024-04-23 11:08:23 -0400

    Added an API request class to clean up Program Class

---
+++ OpenWeatherMapAPI/WeatherApiRequestService.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+    public class WeatherApiRequestService
+    {
+        public async Task CallApi()
+        {
+            string key = File.ReadAllText("appsettings.json");
+
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            while (true)
+            {
+                Console.WriteLine("Enter your ZipCode: ");
+                var zipCode = Console.ReadLine();
+                int num = -1;
+                try
+                {
+                    if (zipCode.Length == 5 && int.TryParse(zipCode, out num))
+                    {
+                        Console.WriteLine("Valid ZipCode!\n");
+                        var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+                        Console.WriteLine($"\nIt is currently {WeatherMap.GetTemp(apiCall)} degrees!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid ZipCode");
+                    }
+                }
+                catch (HttpRequestException)
+                {
+                    Console.WriteLine("\nThe entered ZipCode is not found. Please enter a valid ZipCode.\n");
+                }
+                catch (AggregateException ex)
+                {
+                    Console.WriteLine($"\nAn Error Occurred. {ex.Message} Please enter a valid zipcode\n");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\nAn Error Occurred. {ex.Message} Please enter a valid zipcode\n");
+                }
+
+            }
+        }
+    }
+}
---
+++ OpenWeatherMapAPI/WeatherMap.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string apiCall)
+        {
+            var client = new HttpClient();
+
+            var response = client.GetStringAsync(apiCall).Result;
+
+            var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+
+            return temp;
+        }
+    }
+}
