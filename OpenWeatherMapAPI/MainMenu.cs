fb7ea783507c429e3aa5c56b783697e6a9c93dac
RichardRodi <RichardmRodi@gmail.com>
2024-05-08 15:45:29 -0400

    added search by City and State to title page

--- OpenWeatherMapAPI/MainMenu.cs
+++ OpenWeatherMapAPI/MainMenu.cs
@@ -10,7 +10,7 @@
             {
                 string prompt = ($"\n\n\n\t\t\t ☁  ☁  ☁  Open Weather Map API ☁  ☁  ☁ \n\n\n" +
                     $"{ArtAssets.Sunset}\n\n\n" +
-                    $"\t\tFind the Current Temperature in the USA by ZipCode or by State.\n");
+                    $"\t\tFind the Current Temperature in the USA by ZipCode or by City and State.\n");
 
                 string[] options = { "1.Find by Zipcode.", "2.Find by City and State.",};
                 Menu menu = new Menu(prompt, options);
